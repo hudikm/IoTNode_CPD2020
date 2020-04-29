83496648ea9805d9e304673d7b374614495a2413
Martin Hudik <hudikm@gmail.com>
2020-04-29 18:30:11 +0200

    1.3.a Vytvorenie požiadavky na získanie údajov o aktuálnom počasí

--- src/main/java/sk/fri/uniza/App.java
+++ src/main/java/sk/fri/uniza/App.java
@@ -1,10 +1,22 @@
 package sk.fri.uniza;
 
+import retrofit2.Call;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello IoT!
  */
 public class App {
     public static void main(String[] args) {
+        IotNode iotNode = new IotNode();
+        // Vytvorenie požiadavky na získanie údajov o aktuálnom počasí z
+        // meteo stanice s ID: station_1
+        Call<Map<String, String>> currentWeather =
+                iotNode.getWeatherStationService()
+                        .getCurrentWeatherAsMap("station_1");
+
 
     }
 }
